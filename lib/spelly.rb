aefe8e13a0dcbb63647572233f3dfe4aef0df7d4
Paul Durbin <hello@pauldurbin.co.uk>
2021-03-17 12:17:24 +0000

    Update to use the latest Hunspell

--- lib/spelly.rb
+++ lib/spelly.rb
@@ -1,23 +1,30 @@
 # frozen_string_literal: true
 
-require 'hunspell-ffi'
+require 'ffi/hunspell'
 
 class Spelly
   attr_accessor :dict
 
-  def initialize(language)
-    path = File.expand_path('../lib/dict', File.dirname(__FILE__))
-    # path = "#{Gem.loaded_specs['spelly'].full_gem_path}/lib/dict"
-    @dict = Hunspell.new(path, language)
+  def initialize(language: 'en_GB')
+    @path = File.expand_path('../lib/dict', File.dirname(__FILE__))
+    @language = language
   end
 
   def spell_check(words)
     results = []
     words.each do |word|
-      unless @dict.check?(word)
-        results << { word: word, suggest: @dict.suggest(word) }
+      unless dictionary.check?(word)
+        results << { word: word, suggest: dictionary.suggest(word) }
       end
     end
     results
+  end
+
+  attr_accessor :path, :language
+
+  private
+
+  def dictionary
+    @dictionary ||= FFI::Hunspell.new(path, language)
   end
 end
